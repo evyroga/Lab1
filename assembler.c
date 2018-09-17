e9bb40cba836aadf50bce26c1ae9dfc41d99befa
vivianmnguyen <vivian.m.nguyen@utexas.edu>
2018-09-17 15:27:25 -0700

    Reads in input file, writes to output file

--- assembler.c
+++ assembler.c
@@ -5,12 +5,20 @@
  *      Author: Vivian
  */
 #include <stdio.h>
-
+#include <stdlib.h>
 
 void main(int argc, char *argv[]) {
 	char *input = argv[1];
+	char *output = argv[2];
+	// read in file
 	FILE *inputFile = fopen(input, "r");
+	// create output file
+	FILE *newFilePointer = fopen(output, "w");
 
-
+	// Parse lines from input file and write to output file
+	char lineBuffer[255];
+	while(fgets(lineBuffer, 255, (FILE*) inputFile)) {
+		fprintf(newFilePointer, lineBuffer);
+	}
 	exit(0);
 }
