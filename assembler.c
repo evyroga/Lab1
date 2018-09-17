a5220ea5a4d7d11e1a8abe33b8716eeffd42f15c
vivianmnguyen <vivian.m.nguyen@utexas.edu>
2018-09-17 15:00:13 -0700

    file io

--- assembler.c
+++ assembler.c
@@ -4,9 +4,13 @@
  *  Created on: Sep 17, 2018
  *      Author: Vivian
  */
+#include <stdio.h>
 
 
-void main(void) {
-	printf("Hello world!");
+void main(int argc, char *argv[]) {
+	char *input = argv[1];
+	FILE *inputFile = fopen(input, "r");
+
+
 	exit(0);
 }
